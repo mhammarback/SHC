12d89d21a91bdc594aa2485391d4eb17912a3ccf
Moa <63230455+mhammarback@users.noreply.github.com>
2020-12-17 14:27:24 +0100

    more changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import { NavLink } from 'react-router-dom'
 export const Header = () => {
     return(
         <header>
-           
             <nav>
             <NavLink to ="/" className="nav-links">
                 Home
